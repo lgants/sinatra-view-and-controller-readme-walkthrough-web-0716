eb06ba5aaec8463a2aa8091c045ed3ec3485f6b1
lgants <lgants@gmail.com>
2016-08-11 18:07:10 -0400

    sinatra view and controller readme lab complete -ltg

--- app.rb
+++ app.rb
@@ -7,11 +7,15 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    #an instance variable in a controller method (route) lets the contents become 'visible' to the erb file where it is rendered
+    @reversed_string = original_string.reverse
     erb :reversed
   end
 
   get '/friends' do
-    #write your code here
+    @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+    erb :friends
   end
 
 end
